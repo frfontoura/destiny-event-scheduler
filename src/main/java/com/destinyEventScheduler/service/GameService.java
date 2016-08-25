7ea21fcf0f90c069bd326fce89e3c67b3615752e
Fernando <frfont@gmail.com>
2016-08-25 13:16:08 -0300

    Update Games

--- src/main/java/com/destinyEventScheduler/service/GameService.java
+++ src/main/java/com/destinyEventScheduler/service/GameService.java
@@ -33,8 +33,8 @@ public class GameService {
 	@Autowired
 	private EvaluationService evaluationService;
 	
+	@Transactional
 	public Game getGameById(Long gameId){
-		gameRepository.updateGamesStatusWaiting();
 		return gameRepository.findOne(gameId);
 	}
 	
